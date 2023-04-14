210041f957d36230565d0c1f90343b214a01de69
Rafael Rizzo <rafaelbreschi4@gmail.com>
2023-04-13 22:24:19 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo do site</title>
+</head>
+
+<body>
+    <nav>Navbar fixed bottom</nav>
+
+    <main>
+
+        <header>
+            <h1>Hello World</h1>
+        </header>
+
+        <section>
+            <p>Content of the site</p>
+        </section>
+
+    </main>
+
+    <footer>footer of the site</footer>
+</body>
+
+</html>
